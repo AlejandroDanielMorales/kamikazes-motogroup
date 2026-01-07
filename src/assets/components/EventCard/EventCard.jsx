a314ebf78362ed6db538a616089a09789a860720
AlejandroDanielMorales <116897330+AlejandroDanielMorales@users.noreply.github.com>
2026-01-06 21:59:41 -0300

    upgrading #1
    se realizan mejoras en las vistas de las targetas de eventos

--- src/assets/components/EventCard/EventCard.jsx
+++ src/assets/components/EventCard/EventCard.jsx
@@ -1,32 +1,50 @@
 import "./EventCard.css";
 
 function EventCard({ event }) {
+  const mainImage = event.images?.[0];
+
   return (
     <div className="event-card card">
+      {mainImage && (
+        <div className="event-card-image">
+          <img src={mainImage} alt={event.title} />
+        </div>
+      )}
+
       <h3>{event.title}</h3>
 
       {event.description && <p>{event.description}</p>}
+
+      <h4>Fecha</h4>
 
       <div className="event-card-meta">
         <span>📅 {new Date(event.date).toLocaleDateString()}</span>
         <span className={`event-status ${event.status}`}>
           {event.status}
         </span>
-      </div>
 
+      </div>
+      <h4>Primer lugar de encuentro</h4>
       {event.meetingAddress && (
         <div className="event-address">
-          📍 {event.meetingAddress}
+          <p>📍 {event.meetingAddress}</p>
         </div>
+        
       )}
 
+      <h4>Paradas y encuentros</h4>
       {event.stops?.length > 0 && (
         <ul className="event-stops">
           {event.stops.map((stop, i) => (
-            <li key={i}>🏁 {stop.name}</li>
+            <>
+            <li key={stop._id || i}>🏁 {stop.name}</li>
+            <li key={stop._id || i}>📝 {stop.description}</li>
+
+            </>
           ))}
         </ul>
       )}
+      <button>Ver detalles</button>
     </div>
   );
 }
